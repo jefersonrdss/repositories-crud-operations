bdb3c46d976439b26b0c16c0d98b6729d1902812
Jeferson Rodrigues <jefersonr.santos@outlook.com>
2021-08-26 20:42:26 -0300

    finished route put for update repository

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ app.use(express.json());
 
 const repositories = [];
 
+
+// listar repositorios
 app.get("/repositories", (request, response) => {
     return response.json(repositories);
 });
 
+// cadastrar repositorio
 app.post("/repositories", (request, response) => {
 
     // dados da requisicao
@@ -31,19 +34,28 @@ app.post("/repositories", (request, response) => {
     return response.status(201).json(repository);
 });
 
+// atualizar repositorio
 app.put("/repositories/:id", (request, response) => {
+    
+    // dados da requisicao
     const { id } = request.params;
     const updatedRepository = request.body;
 
-    repositoryIndex = repositories.findindex(repository => repository.id === id);
+    // remover a propriedade likes caso tenha sido passada
+    if (updatedRepository.likes){
+        delete updatedRepository.likes;
+    }
 
+    // verificar se repositorio existe
+    repositoryIndex = repositories.findIndex(repository => repository.id === id);
     if (repositoryIndex < 0) {
         return response.status(404).json({ error: "Repository not found" });
     }
 
+    // merge dos objetos (atual e novo para atualizar)
     const repository = { ...repositories[repositoryIndex], ...updatedRepository };
 
-    repositories[repositoryIndex] = repository;
+    repositories[repositoryIndex] = repository; // atualizando repositorio na lista
 
     return response.json(repository);
 });
